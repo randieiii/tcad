2d1e4aea79a309d696710c0ad3fd10328c745521
Andrei Klimenko <digitalconfusedbottle@gmail.com>
2020-02-18 20:02:11 +0200

    Adding anylyzer

---
+++ analyzer/tarnado.py
@@ -0,0 +1,18 @@
+import tornado.ioloop
+import tornado.web
+import tornado
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        items = ["Item 1", "Item 2", "Item 3"]
+        self.render("template.html", title="My title", items=items)
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
